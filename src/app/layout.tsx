aa100659f7628123dbb63205361906779ea6a755
Matt Wills <mattwillscode@gmail.com>
2024-05-22 11:18:19 -0400

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const font = JetBrains_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "mwills.dev",
+  description: "Portfolio website for Software Engineer Matthew Wills",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
